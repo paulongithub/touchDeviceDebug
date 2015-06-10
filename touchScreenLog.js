9c7bbcb42f936f5ea09b25025da9fbd3ab8d0a59
paulongithub <psdslh@gmail.com>
2015-06-10 11:37:50 +0100

    add hide and delete buttons

--- touchScreenLog.js
+++ touchScreenLog.js
@@ -33,48 +33,29 @@
 
     $(function(){
 
-        $('body').append("<div data-log><div data-log-count><span>log count:&nbsp;</span><span class=\"count\"></span></div></div>");
-
-        $('[data-log]').css({
-            width:'100%',
-            position:'fixed', 
-            height:'50%',
-            top:'50%',
-            overflow:'scroll',
-            background:'#000',
-            color:'#0f0',
-            'font-size':'10px',
-            'font-family':'monospace',
-            'z-index':999999,
-            '-webkit-tap-highlight-color':'rgba(0,0,0,0)',
-            '-webkit-touch-callout': 'none',
-            '-webkit-user-select': 'none',
-            '-khtml-user-select': 'none',
-            '-moz-user-select': 'none',
-            '-ms-user-select': 'none',
-            'user-select': 'none'
-        });
-
-        $('[data-log-count]').css({
-            'position':'absolute',
-            'right':0,
-            'top':0
-        });
+        //html elements
+        $('body').append("<div data-log><div data-log-delete data-cross='x'></div><div data-log-hide data-arrow='\u2193'></div><div data-log-count><span>log count:&nbsp;</span><span class=\"count\"></span></div></div>");
+        //css
+        $('body').append("<style type='text/css'>[data-log-delete]{width:10px;height:10px;background:#0f0;color:black;position:absolute;left:0;top:0;cursor:pointer}[data-log-delete]:before{content:attr(data-cross);position:absolute;top:-2px;left:2px;}[data-log-hide]{width:10px;height:10px;background:#0f0;color:black;position:absolute;left:50%;top:0;cursor:pointer}[data-log-hide]:before{content:attr(data-arrow);position:absolute;top:-2px;left:2px;}[data-log-count]{position:absolute;right:0;top:0}[data-log]{width:100%;position:fixed;height:25%;top:75%;overflow:scroll;background:#000;color:#0f0;font-size:10px;font-family:monospace;z-index:999999;-webkit-tap-highlight-color:rgba(0,0,0,0);-webkit-touch-callout:none;-webkit-user-select:none;-khtml-user-select:none;-moz-user-select:none;-ms-user-select:none;user-select:none}</style>");
 
 	    var logTimer = 0;
 	    
-	    $('[data-log]').on('touchstart mousedown touchmove mousemove',function(){
-		    logTimer = new Date().getTime();
+	    $('[data-log-delete]').on('touchstart mousedown',function(){
+		     $('[data-log]').children('p').remove();
 	    });
 		
-	    $('[data-log]').on('touchend mouseup',function(){
-		    var now = new Date().getTime();
-		    
-		    if (logTimer && (now - 1000) > logTimer) {
-		    logTimer = 0;
-		    $('[data-log]').children('p').remove();
-		    }
-	    });
+
+        $('[data-log-hide]').on('touchstart mousedown',function(){
+            if ($(this).hasClass('log-hidden')) {
+                $(this).attr('data-arrow','\u2193').removeClass('log-hidden');
+                $('[data-log]').stop().animate({'top':'75%'});
+                return;
+            }
+
+            $(this).attr('data-arrow','\u2191').addClass('log-hidden');
+            $('[data-log]').stop().animate({'top':'98%'});
+
+        });
 
     });
 
